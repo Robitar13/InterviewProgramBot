74858fb10eefbbee91f3ab6e0ee198cf32ec5f04
Robitar13 <167028556+Robitar13@users.noreply.github.com>
2025-04-19 11:52:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import random
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")  # Или вставь свой токен напрямую, если тестируешь локально
-bot = telebot.TeleBot(8162107934:AAFI7VCgfRZjBCwAOsZK_2SE-TvqQDwgkFY)
+bot = telebot.TeleBot()
 
 DATA_DIR = "data"
 SESSIONS_FILE = "user_sessions.json"
